453c2a4a15270e21b876f95e52eb59a5e7b1327d
Michael Laderman <mlader@brandeis.edu>
2012-09-02 20:27:49 -0400

    Added Contact Form

--- splash-page/js/plugins.js
+++ splash-page/js/plugins.js
@@ -9,3 +9,39 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 
+
+(function($) {
+	$.fn.spin = function(opts, color) {
+		var presets = {
+			"tiny": { lines: 8, length: 2, width: 2, radius: 3 },
+			"small": { lines: 8, length: 4, width: 3, radius: 5 },
+			"large": { lines: 10, length: 8, width: 4, radius: 8 }
+		};
+		if (Spinner) {
+			return this.each(function() {
+				var $this = $(this),
+					data = $this.data();
+
+				if (data.spinner) {
+					data.spinner.stop();
+					delete data.spinner;
+				}
+				if (opts !== false) {
+					if (typeof opts === "string") {
+						if (opts in presets) {
+							opts = presets[opts];
+						} else {
+							opts = {};
+						}
+						if (color) {
+							opts.color = color;
+						}
+					}
+					data.spinner = new Spinner($.extend({color: $this.css('color')}, opts)).spin(this);
+				}
+			});
+		} else {
+			throw "Spinner class not available.";
+		}
+	};
+})(jQuery);
--- splash-page/js/script.js
+++ splash-page/js/script.js
@@ -6,9 +6,13 @@
 $(document).ready(function(){
 
 scrolling("navbar");
+collapse("contact-button",500);
+spinz("contact-form")
+
 
 
 });
+
 $("a.scroll").click(function() {
 	
 		var path = $(this).attr("id").split("_")[1];			
@@ -20,3 +24,61 @@ function scrolling(id)
 {
 	$("html,body").animate({scrollTop: $("#" + id).offset().top - 60}, 1000);
 }
+
+function collapse(id,duration){
+	$("#"+id).click(function(){
+		$("#contact").toggleClass("collapsed",duration);
+		$("#"+id).children().toggleClass("icon-minus-sign");
+		$("#"+id).children().toggleClass("icon-plus-sign");
+	});
+
+}
+
+
+
+
+
+function spin(target){
+var opts = {
+  lines: 13, // The number of lines to draw
+  length: 18, // The length of each line
+  width: 8, // The line thickness
+  radius: 18, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  color: '#000', // #rgb or #rrggbb
+  speed: 1.2, // Rounds per second
+  trail: 72, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: 'auto', // Top position relative to parent in px
+  left: 'auto' // Left position relative to parent in px
+};
+var target = document.getElementById(target);
+
+var spinner = new Spinner(opts).spin(target);
+
+}
+
+
+function spinz(id){
+	$('#'+id).submit(function() {
+    // $('#loading_image').show(); // show animation
+    spin(id);
+    $(':submit',this).attr('disabled','disabled');
+    $.ajax({
+	    url: '..',
+	    type: '..',
+	    complete: function() {
+	        // request is complete, regardless of error or success, so hide image
+	        $('#'+id).stop();
+	    }
+	});
+    return true; // allow regular form submission
+	});
+}
+
+
+
